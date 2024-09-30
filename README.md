e304be0f9d5fcb4490a7bb6dfe10cc3f46847804
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-09-30 05:45:56 -0300

    added helmet

---
+++ README.md
@@ -0,0 +1,1 @@
+# Node auth and security example
--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import path from "path";
 import https from "https";
 import express from "express";
 import { fileURLToPath } from "url";
+import helmet from "helmet";
 
 const PORT = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app = express();
+app.use(helmet());
 
 app.use(express.static("public"));
 
