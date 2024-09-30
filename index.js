d54692b20a6846e181960f7bc0263f6507d66735
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-09-30 01:01:17 -0300

    initial auth demo code

---
+++ index.js
@@ -0,0 +1,33 @@
+import fs from "fs";
+import path from "path";
+import https from "https";
+import express from "express";
+import { fileURLToPath } from "url";
+
+const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/secret", (req, res) => {
+  return res.send("Your secret goes here");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
