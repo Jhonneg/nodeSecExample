edf071c8e4875698b963a8fc8aa37af047cd2886
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-10-01 19:03:45 -0300

    added passport js, initial auth code

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ import https from "https";
 import express from "express";
 import { fileURLToPath } from "url";
 import helmet from "helmet";
+import { error } from "console";
 
 const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,9 +19,24 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(helmet());
 
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true;
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in",
+    });
+  }
+  next();
+}
+
+app.get("/auth/google", (req, res) => {});
+app.get("/auth/google/redirect", (req, res) => {});
+
+app.get("/auth/logout", (req, res) => {});
+
 app.use(express.static("public"));
 
-app.get("/secret", (req, res) => {
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your secret goes here");
 });
 
