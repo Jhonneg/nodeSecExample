5c4ffa505090f3e90d675931176f7cd076309d4d
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-10-01 20:07:33 -0300

    google sign in on

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import fs from "fs";
 import path from "path";
 import https from "https";
+import helmet from "helmet";
 import express from "express";
 import { fileURLToPath } from "url";
-import helmet from "helmet";
-import { error } from "console";
+import passport from "passport";
+import { Strategy } from "passport-google-oauth20";
 
 const PORT = 3000;
 
@@ -13,11 +14,25 @@ const config = {
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
 
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/redirect",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google Profile", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(helmet());
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true;
@@ -29,8 +44,24 @@ function checkLoggedIn(req, res, next) {
   next();
 }
 
-app.get("/auth/google", (req, res) => {});
-app.get("/auth/google/redirect", (req, res) => {});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
+
+app.get(
+  "/auth/google/redirect",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("google called us back!");
+  }
+);
 
 app.get("/auth/logout", (req, res) => {});
 
@@ -38,6 +69,10 @@ app.use(express.static("public"));
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your secret goes here");
+});
+
+app.get("/failure", checkLoggedIn, (req, res) => {
+  return res.send("Failed to log in!");
 });
 
 app.get("/", (req, res) => {
